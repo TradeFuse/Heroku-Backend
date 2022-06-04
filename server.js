b987367578e9b923269cc73a24c599983450404c
TradersIQ <62960583+TradersIQ@users.noreply.github.com>
2022-06-04 11:24:02 -0700

    V2 REVERT BECAUSE IT JUST DECIDED TO STOP WORKINGH\

--- server.js
+++ server.js
@@ -2,7 +2,6 @@
 
 const express = require("express");
 const cors = require('cors');
-const stripe = require('stripe')(process.env.PLATFORM_SECRET_KEY);
 
 // Constants
 const PORT = process.env.PORT || 80;
@@ -20,36 +19,13 @@ app.use((req, res, next) => {
   next();
 });
 app.use(cors());
-app.use(express.json());
-app.listen(PORT);
-
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
 app.post("/", (req, res) => {
-  const bodyData = res.json({requestBody: req.body});
-  if (bodyData.requestBody.action === "createStripeCustomer") {
-    const customer = await stripe.customers.create({
-      name: "",
-      email: "",
-      metadata: {
-        Logins: 1,
-        "Last Login": String(moment(today).format("MM/DD/YYYY hh:mm:ss A")),
-        Trades: 0,
-        "Shared Trades": 0,
-        Tier: "Free",
-        "Storage Used": `0 KB`
-      },
-    });
-    const resCustomer = res.json(customer);
-    res.send(resCustomer);
-  }
-
+  res.send("POST Request Called");
 });
 
-app.post("/createStripeCustomer", (req, res) => {
-  res.send("create stripe customer");
-});
-
+app.listen(PORT);
 console.log(`Running on http://${PORT}`);

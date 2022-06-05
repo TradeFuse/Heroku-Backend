414d54909cc2dbfa91e9ef6ecd2acb99bc736c2a
TradersIQ <62960583+TradersIQ@users.noreply.github.com>
2022-06-05 10:35:23 -0700

    try of discord bot

--- modules/discordBot.js
+++ modules/discordBot.js
@@ -3,6 +3,9 @@ const { Client, Intents } = require("discord.js");
 const discordClient = new Client({
   intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
 });
+const prefix = require("discord-prefix");
+let defaultPrefix = "!";
+
 module.exports = async function discordBot() {
   discordClient.on("ready", () => {
     console.log("Discord bot ready.");
@@ -10,7 +13,7 @@ module.exports = async function discordBot() {
 
   discordClient.on("guildMemberRemove", function (member) {
     const channel = member.guild.channels.cache.find(
-      (ch) => ch.name === "community"
+      (ch) => ch.name === "logbook"
     );
     channel.send(`${member} has left the party.`);
   });
@@ -19,7 +22,7 @@ module.exports = async function discordBot() {
     const channel = message.guild.channels;
     const usernamebuff = message.author;
     channel
-      .find("name", "community")
+      .find("name", "logbook")
       .send(`${usernamebuff} is deleting a lot of messages. Looks suspicious.`);
   });
 
@@ -31,12 +34,37 @@ module.exports = async function discordBot() {
   });
 
   discordClient.on("guildUnavailable", (guild) => {
-    const channel = guild.channels.cache.find((ch) => ch.name === "community");
+    const channel = guild.channels.cache.find((ch) => ch.name === "logbook");
     channel.send(
       `Woah, looks like the ${guild} server is down. Check again later for continuation of awesomeness.`
     );
   });
 
-  discordClient.login(process.env.DISCORD_LOGIN_KEY);
-}
+  discordClient.on("message", (message) => {
+    /*     //stop code execution if message is received in DMs
+    if (!message.guild) return; */
 
+    //get the prefix for the discord server
+    let guildPrefix = prefix.getPrefix(message.guild.id);
+
+    //set prefix to the default prefix if there isn't one
+    if (!guildPrefix) guildPrefix = defaultPrefix;
+
+    //rest of the message event
+    //let args = message.content.slice(guildPrefix.length).split(" ");
+    const channel = message.channel.id;
+    if (channel === "bug-and-feedback") {
+      const channelNew = guild.channels.cache.find(
+        (ch) => ch.name === "user-feedback"
+      );
+      if (!message.content.startsWith(guildPrefix)) {
+      } else {
+        channelNew.send(message);
+      }
+    }
+
+    return;
+  });
+
+  discordClient.login(process.env.DISCORD_LOGIN_KEY);
+};

ec0aabb37f18eecdf7e9740ee00bac35a0a9aa43
TradersIQ <62960583+TradersIQ@users.noreply.github.com>
2022-06-09 08:12:18 -0700

    change to 5000, added guild_members

--- modules/discordBot.js
+++ modules/discordBot.js
@@ -3,6 +3,7 @@ const { Client, Intents } = require("discord.js");
 const discordClient = new Client({
   intents: [
     Intents.FLAGS.GUILDS,
+    Intents.FLAGS.GUILD_MEMBERS,
     Intents.FLAGS.GUILD_MESSAGES,
     Intents.FLAGS.DIRECT_MESSAGES,
   ],
--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const getCustomer = require("./utils/getStripeCustomer");
 const updateCustomer = require("./utils/updateStripeCustomer");
 
 // Constants
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 //const HOST = "0.0.0.0";
 
 // App

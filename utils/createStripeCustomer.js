01cee4af77aebda5a782f1505dac8ec28e5f1871
TradersIQ <62960583+TradersIQ@users.noreply.github.com>
2022-06-14 12:34:21 -0700

    try catch wrapper around create

--- utils/createStripeCustomer.js
+++ utils/createStripeCustomer.js
@@ -1,21 +1,25 @@
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 module.exports = async function createCustomer(bodyData) {
-  const customer = await stripe.customers.create({
-    name: "",
-    email: "",
-    metadata: {
-      Logins: 1,
-      "Last Login": bodyData.data["Last Login"],
-      "Last Session": bodyData.data["Last Session"],
-      Trades: 0,
-      "Shared Trades": 0,
-      Tier: "Free",
-      "Storage Used": `2.94 KB`, // default data usage
-    },
-  });
-  if (!customer) {
-    throw (`Didn't create customer.`)
+  let customer = "";
+  try {
+    customer = await stripe.customers.create({
+      name: "",
+      email: "",
+      metadata: {
+        Logins: 1,
+        "Last Login": bodyData.data["Last Login"],
+        "Last Session": bodyData.data["Last Session"],
+        Trades: 0,
+        "Shared Trades": 0,
+        Tier: "Free",
+        "Storage Used": `2.94 KB`, // default data usage
+      },
+    });
+  } catch {
+    if (!customer) {
+      throw (`Couldn't create customer.`)
+    }
   }
   return customer;
 };

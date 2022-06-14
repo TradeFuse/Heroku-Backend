de371139aa16a597b30ee0f0e87aa3b7d64a3536
TradersIQ <62960583+TradersIQ@users.noreply.github.com>
2022-06-14 12:22:58 -0700

    updat6e to updatestripecustomer

--- utils/updateStripeCustomer.js
+++ utils/updateStripeCustomer.js
@@ -2,22 +2,26 @@ const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 module.exports = async function updateCustomer(bodyData) {
   const updatecustomerId = bodyData.data.customerId;
-  const updatedCustomer = await stripe.customers.update(updatecustomerId, {
-    name: bodyData.data.name,
-    email: bodyData.data.email,
-    metadata: {
-      Logins: bodyData.data["Logins"],
-      "Last Login": bodyData.data["Last Login"],
-      "Last Session": bodyData.data["Last Session"],
-      Trades: bodyData.data["Trades"],
-      "Shared Trades": bodyData.data["Shared Trades"],
-      Tier: bodyData.data["Tier"],
-      "Storage Used": bodyData.data["Storage Used"],
-    },
-  });
-  if (!updatedCustomer) {
-    throw (`${updatedCustomer} is invalid`)
+  let updatedCustomer = "";
+  try {
+    updatedCustomer = await stripe.customers.update(updatecustomerId, {
+      name: bodyData.data.name,
+      email: bodyData.data.email,
+      metadata: {
+        Logins: bodyData.data["Logins"],
+        "Last Login": bodyData.data["Last Login"],
+        "Last Session": bodyData.data["Last Session"],
+        Trades: bodyData.data["Trades"],
+        "Shared Trades": bodyData.data["Shared Trades"],
+        Tier: bodyData.data["Tier"],
+        "Storage Used": bodyData.data["Storage Used"],
+      },
+    });
+  } catch {
+    if (!updatedCustomer) {
+      throw `${updatedCustomer} is invalid`;
+    }
   }
-  return updatedCustomer;
 
+  return updatedCustomer;
 };

4f8951fc7e8853cdd527e24e4a63cc2b18470c23
TradersIQ <62960583+TradersIQ@users.noreply.github.com>
2022-06-26 10:43:13 -0700

    try with post

--- brokerHandling/robinhood/initializeRobinhood.js
+++ brokerHandling/robinhood/initializeRobinhood.js
@@ -12,6 +12,34 @@ module.exports = async function initializeRobinhood(bodyData, req) {
     };
   };
 
+
+  const step1Robinhood = async () => {
+    const dataIn = {
+      action: "initializeRobinhood",
+      data: {
+        username: email,
+        password: password,
+      },
+    };
+    const response = await fetch("api.robinhood.com/api-token-auth/", {
+      method: "POST", // *GET, POST, PUT, DELETE, etc.
+      headers: {
+        Host: "api.robinhood.com",
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods":
+          "OPTIONS, DELETE, POST, GET, PATCH, PUT",
+        "Access-Control-Allow-Headers": "Content-Type",
+      },
+      body: JSON.stringify(dataIn),
+    }).catch((err) => {
+      throw err;
+    });
+    return response.json(); // parses JSON response into native JavaScript objects
+  };
+  const mfaData = await step1Robinhood();
+  console.log(mfaData)
   var Robinhood = robinhood(credentials(email, password), (err, data) => {
     if (err) {
     } else {

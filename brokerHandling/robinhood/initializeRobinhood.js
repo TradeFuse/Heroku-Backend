c809232e27b1af109313ce134ee4476894b38996
TradersIQ <62960583+TradersIQ@users.noreply.github.com>
2022-06-26 09:01:27 -0700

    try to make functions that retrun something out "Robinhood"

--- brokerHandling/robinhood/initializeRobinhood.js
+++ brokerHandling/robinhood/initializeRobinhood.js
@@ -12,6 +12,44 @@ module.exports = async function initializeRobinhood(bodyData) {
     };
   };
   let returnObj = {};
+  
+  const RobinhoodTry = robinhood(credentials(email, password), (err, data) => {
+    return { err, data, RobinhoodTry };
+  });
+
+  const data = RobinhoodTry.data;
+  const error = RobinhoodTry.err;
+  const RobinhoodTry2 = RobinhoodTry.RobinhoodTry
+  console.log(RobinhoodTry2);
+  if (data && data.mfa_required) {
+    var mfa_code = mfaCode; // set mfa_code here
+
+    const token = RobinhoodTry2.set_mfa_code(mfa_code, async () => {
+      return RobinhoodTry2.auth_token();
+    });
+
+    const orders =
+      token &&
+      RobinhoodTry2.orders(null, function (err, response, body) {
+        if (err) {
+          throw err;
+        } else {
+          return body;
+        }
+      });
+    propsToChange = {
+      linkedBrokerInfo: {
+        broker: "robinhood",
+        token: token,
+        /*                         askforcode: checked,
+         */
+      },
+    };
+    returnObj.propsToChange = propsToChange;
+    returnObj.orders = orders;
+  } else {
+  }
+
   var Robinhood = robinhood(credentials(email, password), (err, data) => {
     if (err) {
     } else {

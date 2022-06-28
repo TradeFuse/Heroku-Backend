437e725ee0d3f287192d62dfbc3410fc05497549
TradersIQ <62960583+TradersIQ@users.noreply.github.com>
2022-06-27 23:07:36 -0700

    added Authorization header

--- brokerHandling/robinhood/getRobinhoodOrders.js
+++ brokerHandling/robinhood/getRobinhoodOrders.js
@@ -3,23 +3,28 @@ const fetch = (...args) =>
 
 module.exports = async function getRobinhoodOrders(bodyData, req) {
   let returnObj = {};
+  const _authToken = bodyData.data["token"];
 
   const getRobinhoodO = async () => {
     let options = {
-      updated_at: '2017-08-25',
+      updated_at: "2017-08-25",
     };
     const headerOptions = queryString.stringify(options);
-    const response = await fetch("https://api.robinhood.com/orders/" + headerOptions, {
-      method: "GET", // *GET, POST, PUT, DELETE, etc.
-      headers: {
-        Host: "api.robinhood.com",
-        Accept: "application/json",
-        "Content-Type": "application/json",
-        "Accept-Encoding": "gzip, deflate",
-        Referer: "https://robinhood.com/",
-        Origin: "https://robinhood.com",
-      },
-    }).catch((err) => {
+    const response = await fetch(
+      "https://api.robinhood.com/orders/" + headerOptions,
+      {
+        method: "GET", // *GET, POST, PUT, DELETE, etc.
+        headers: {
+          Host: "api.robinhood.com",
+          Accept: "application/json",
+          "Content-Type": "application/json",
+          "Accept-Encoding": "gzip, deflate",
+          Referer: "https://robinhood.com/",
+          Origin: "https://robinhood.com",
+          Authorization: "Bearer " + _authToken,
+        },
+      }
+    ).catch((err) => {
       throw err;
     });
     return response.json(); // parses JSON response into native JavaScript objects

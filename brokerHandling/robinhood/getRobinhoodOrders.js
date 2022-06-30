17f289d4893e1eaa0e2799b0ab4f23b45ff1c2a0
TradersIQ <62960583+TradersIQ@users.noreply.github.com>
2022-06-29 19:26:32 -0700

    do pagination with robinhood

--- brokerHandling/robinhood/getRobinhoodOrders.js
+++ brokerHandling/robinhood/getRobinhoodOrders.js
@@ -3,38 +3,48 @@ const fetch = (...args) =>
   import("node-fetch").then(({ default: fetch }) => fetch(...args));
 
 module.exports = async function getRobinhoodOrders(bodyData, req) {
-  let returnObj = {};
   const _authToken = bodyData.data["token"];
 
-  const getRobinhoodO = async () => {
-    let options = {
-      "updated_at[gte]": "2017-08-25",
-    };
-    const headerOptions = '?' + queryString.stringify(options);
-    const response = await fetch(
-      "https://api.robinhood.com/orders/" + headerOptions,
-      {
-        method: "GET", // *GET, POST, PUT, DELETE, etc.
-        headers: {
-          Host: "api.robinhood.com",
-          Accept: "application/json",
-          "Content-Type": "application/json",
-          "Accept-Encoding": "gzip, deflate",
-          Referer: "https://robinhood.com/",
-          Origin: "https://robinhood.com",
-          Authorization: "Bearer " + _authToken,
-        },
-      }
-    ).catch((err) => {
+  let allorders = [];
+  let isNextExist = true;
+
+  // INITAL HEADER OPTIONS
+  let options = {
+    "updated_at[gte]": "2017-08-25",
+  };
+  const headerOptions = "?" + queryString.stringify(options);
+  let nextURL = "https://api.robinhood.com/orders/" + headerOptions;
+  // -----------------
+
+  const getRobinhoodO = async (url) => {
+    const response = await fetch(url, {
+      method: "GET", // *GET, POST, PUT, DELETE, etc.
+      headers: {
+        Host: "api.robinhood.com",
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        "Accept-Encoding": "gzip, deflate",
+        Referer: "https://robinhood.com/",
+        Origin: "https://robinhood.com",
+        Authorization: "Bearer " + _authToken,
+      },
+    }).catch((err) => {
       throw err;
     });
     return response.json(); // parses JSON response into native JavaScript objects
   };
 
-  const ordersResponse = await getRobinhoodO();
-  if (ordersResponse) {
-    returnObj = ordersResponse;
+  while (isNextExist) {
+    const ordersResponse = await getRobinhoodO(nextURL);
+    if (ordersResponse) {
+      allorders.push(ordersResponse.results);
+      if (!ordersResponse.next) {
+        isNextExist = false;
+      } else {
+        nextURL = ordersResponse.next;
+      }
+    }
   }
 
-  return returnObj;
+  return allorders;
 };

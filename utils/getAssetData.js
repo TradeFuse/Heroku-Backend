15260a33b33af7233a0aff2b97c95308c94c8fac
TradersIQ <62960583+TradersIQ@users.noreply.github.com>
2022-08-06 08:42:26 -0700

    change to URL to accept key

--- utils/getAssetData.js
+++ utils/getAssetData.js
@@ -12,7 +12,7 @@ module.exports = async function getAssetData(bodyData) {
   };
 
   const response = await fetch(
-    "https://platform.atom.finance/api/2.0/price/snapshot" + "?" + process.env.ATOM_FINANCE_KEY,
+    "https://platform.atom.finance/api/2.0/price/snapshot" + "?api_key=" + process.env.ATOM_FINANCE_KEY,
     options
   ).catch((err) => {
     throw err;

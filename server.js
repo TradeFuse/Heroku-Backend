bd2bce39e368d252a37bd4ecf82621d1adc5837f
TradersIQ <62960583+TradersIQ@users.noreply.github.com>
2022-09-21 09:25:16 -0700

    tried increasing to 100mb

--- server.js
+++ server.js
@@ -36,6 +36,9 @@ app.use((req, res, next) => {
 });
 app.use(cors(corsOptions));
 app.use(express.json());
+app.use(express.json({limit: '100mb'}));
+app.use(express.urlencoded({limit: '100mb'}));
+
 app.listen(PORT);
 
 app.get("/", (req, res) => {

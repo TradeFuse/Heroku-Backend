059e8c4cfa6fe76a6644e939b6cbfb963cb8c556
TradersIQ <62960583+TradersIQ@users.noreply.github.com>
2022-10-23 02:24:40 -0700

    another try for optimization

--- brokerHandling/robinhood/getRobinhoodOrders.js
+++ brokerHandling/robinhood/getRobinhoodOrders.js
@@ -3,6 +3,7 @@ const fetch = (...args) =>
   import("node-fetch").then(({ default: fetch }) => fetch(...args));
 const isIterable = require("../../utils/handleIterator");
 module.exports = async function getRobinhoodOrders(bodyData, req) {
+  console.log(bodyData);
   const _authToken = bodyData.data["token"];
   const _assetClasses = bodyData.data["assetClasses"];
 
@@ -13,6 +14,8 @@ module.exports = async function getRobinhoodOrders(bodyData, req) {
 
   let isNextExistDW = true;
   let isNextExistcrypto = true;
+  let isNextExisttransfers = true;
+
   let isNextExistInstruments = true;
   let isNextExistcard = true;
   let isNextExistach = true;
@@ -47,10 +50,6 @@ module.exports = async function getRobinhoodOrders(bodyData, req) {
     "https://api.robinhood.com/options/events/" + headerOptions;
 
   let cryptoURL = "https://nummus.robinhood.com/orders/" + headerOptions;
-
-  //let instrumentsURL = "https://api.robinhood.com/instruments/";
-  let instrumentsURL =
-    "https://api.robinhood.com/instruments/?cursor=cD1lNzM0YzI5OS0wZjE3LTRhZDAtODRmOS03ZmJiOTg3NmRlYzE%3D";
   // -----------------
   const bearerString = `Bearer ` + _authToken;
   const getRobinhoodO = async (url, id) => {
@@ -84,237 +83,124 @@ module.exports = async function getRobinhoodOrders(bodyData, req) {
       });
     return response; // parses JSON response into native JavaScript objects
   };
-
-  // Get regular orders
-  if (_assetClasses.includes("Stocks")) {
-    while (isNextExist) {
-      const ordersResponse = await getRobinhoodO(ordersURL, "api");
-      if (ordersResponse) {
-        const ordersResults = ordersResponse.results;
-        const ordersMapped = ordersResults?.map((obj) => ({
-          ...obj,
-          rhType: "stock",
-        }));
-        isIterable(ordersMapped) && allorders.push(...ordersMapped);
-        if (
-          !ordersResponse.next ||
-          ordersResponse.next === null ||
-          ordersResponse.next === ""
-        ) {
-          isNextExist = false;
+  const loopFunction = async (loopMap) => {
+    if (loopMap.asset === "") {
+      // bank transfer, ach transfers, ach receieved, wire, transfers, crypto transfers. etc
+      while (loopMap.isNextExistVar) {
+        const bankResponse = await getRobinhoodO(loopMap.url, loopMap.apiType);
+        if (bankResponse) {
+          const bankResults = bankResponse.results;
+          const bankMapped = bankResults?.map((obj) => ({
+            ...obj,
+            rhType: loopMap.type,
+          }));
+          isIterable(bankMapped) && allorders.push(...bankMapped);
+          if (
+            !bankResponse.next ||
+            bankResponse.next === null ||
+            bankResponse.next === ""
+          ) {
+            loopMap.isNextExistVar = false;
+          } else {
+            loopMap.url = ordersResponse.next;
+          }
         } else {
-          ordersURL = ordersResponse.next;
+          loopMap.isNextExistVar = false;
         }
-      } else {
-        isNextExist = false;
-      }
-    }
-  }
-  // Get options orders
-  if (_assetClasses.includes("Options")) {
-    while (isNextExistOptions) {
-      const optionsResponse = await getRobinhoodO(optionsURL, "api");
-      if (optionsResponse) {
-        const optionsResults = optionsResponse.results;
-        const optionsMapped = optionsResults?.map((obj) => ({
-          ...obj,
-          rhType: "option",
-        }));
-        isIterable(optionsMapped) && allorders.push(...optionsMapped);
-        if (
-          !optionsResponse.next ||
-          optionsResponse.next === null ||
-          optionsResponse.next === ""
-        ) {
-          isNextExistOptions = false;
-        } else {
-          optionsURL = optionsResponse.next;
-        }
-      } else {
-        isNextExistOptions = false;
-      }
-    }
-  }
-
-  // Get options events
-  if (_assetClasses.includes("Options")) {
-    while (isNextExistOptionsEvents) {
-      const optionseventsResponse = await getRobinhoodO(
-        optionseventsURL,
-        "api"
-      );
-      if (optionseventsResponse) {
-        const optionseventsResults = optionseventsResponse.results;
-        const optionseventsMapped = optionseventsResults?.map((obj) => ({
-          ...obj,
-          rhType: "optionevent",
-        }));
-        isIterable(optionseventsMapped) &&
-          allorders.push(...optionseventsMapped);
-        if (
-          !optionseventsResponse.next ||
-          optionseventsResponse.next === null ||
-          optionseventsResponse.next === ""
-        ) {
-          isNextExistOptionsEvents = false;
-        } else {
-          optionseventsURL = optionseventsResponse.next;
-        }
-      } else {
-        isNextExistOptionsEvents = false;
-      }
-    }
-  }
-
-  // Get crypto orders
-  if (_assetClasses.includes("Crypto")) {
-    while (isNextExistcrypto) {
-      const cryptoResponse = await getRobinhoodO(cryptoURL, "nummus");
-      if (cryptoResponse) {
-        const cryptoResults = cryptoResponse.results;
-        const cryptoMapped = cryptoResults?.map((obj) => ({
-          ...obj,
-          rhType: "crypto",
-        }));
-        isIterable(cryptoMapped) && allorders.push(...cryptoMapped);
-        if (
-          !cryptoResponse.next ||
-          cryptoResponse.next === null ||
-          cryptoResponse.next === ""
-        ) {
-          isNextExistcrypto = false;
-        } else {
-          cryptoURL = cryptoResponse.next;
-        }
-      } else {
-        isNextExistcrypto = false;
-      }
-    }
-  }
-
-  // Get bank transfers
-  while (isNextExistDW) {
-    const bankResponse = await getRobinhoodO(bankURL, "api");
-    if (bankResponse) {
-      const bankResults = bankResponse.results;
-      const bankMapped = bankResults?.map((obj) => ({
-        ...obj,
-        rhType: "bank transfer",
-      }));
-      isIterable(bankMapped) && allorders.push(...bankMapped);
-      if (
-        !bankResponse.next ||
-        bankResponse.next === null ||
-        bankResponse.next === ""
-      ) {
-        isNextExistDW = false;
-      } else {
-        bankURL = bankResponse.next;
       }
     } else {
-      isNextExistDW = false;
-    }
-  }
-
-  // Get card transactions
-  // by default, exclude this
-  /*   while (isNextExistcard) {
-    const cardResponse = await getRobinhoodO(cardURL, "minerva");
-    if (isNextExistcard) {
-      const cardResults = cardResponse.results;
-      const cardMapped = cardResults.map((obj) => ({
-        ...obj,
-        rhType: "card transaction",
-      }));
-      allorders.push(...cardMapped);
-      if (
-        !cardResponse.next ||
-        cardResponse.next === null ||
-        cardResponse.next === ""
-      ) {
-        isNextExistcard = false;
-      } else {
-        cardURL = cardResponse.next;
+      // Stock, Options, and Crypto orders
+      if (_assetClasses.includes(loopMap.asset)) {
+        while (loopMap.isNextExistVar) {
+          const ordersResponse = await getRobinhoodO(
+            loopMap.url,
+            loopMap.apiType
+          );
+          if (ordersResponse) {
+            const ordersResults = ordersResponse.results;
+            const ordersMapped = ordersResults?.map((obj) => ({
+              ...obj,
+              rhType: loopMap.type,
+            }));
+            isIterable(ordersMapped) && allorders.push(...ordersMapped);
+            if (
+              !ordersResponse.next ||
+              ordersResponse.next === null ||
+              ordersResponse.next === ""
+            ) {
+              loopMap.isNextExistVar = false;
+            } else {
+              loopMap.url = ordersResponse.next;
+            }
+          } else {
+            loopMap.isNextExistVar = false;
+          }
+        }
       }
     }
-  } */
+  };
+  const loopMapArr = [
+    {
+      asset: "Stocks",
+      url: ordersURL,
+      type: "stock",
+      isNextExistVar: isNextExist,
+      apiType: "api",
+    },
+    {
+      asset: "Options",
+      url: optionsURL,
+      type: "option",
+      isNextExistVar: isNextExistOptions,
+      apiType: "api",
+    },
+    {
+      asset: "Options",
+      url: optionseventsURL,
+      type: "optionevent",
+      isNextExistVar: isNextExistOptionsEvents,
+      apiType: "api",
+    },
+    {
+      asset: "Crypto",
+      url: cryptoURL,
+      type: "crypto",
+      isNextExistVar: isNextExistcrypto,
+      apiType: "nummus",
+    },
+    {
+      asset: "",
+      url: bankURL,
+      type: "bank transfer",
+      isNextExistVar: isNextExistDW,
+      apiType: "api",
+    },
+    {
+      asset: "",
+      url: withdrawalURL,
+      type: "ach",
+      isNextExistVar: isNextExistach,
+      apiType: "api",
+    },
+    {
+      asset: "",
+      url: receivedURL,
+      type: "ach received",
+      isNextExistVar: isNextExistachreceived,
+      apiType: "api",
+    },
+    {
+      asset: "",
+      url: transfersURL,
+      type: "transfers",
+      isNextExistVar: isNextExisttransfers,
+      apiType: "bonfire",
+    },
+  ];
+  const functionArray = loopMapArr.map(
+    async (loopMap) => await loopFunction(loopMap)
+  );
+  await Promise.all(functionArray);
 
-  // Get card transfers
-  /*   const cardTransferResponse = await getRobinhoodO(cardTransferURL, "minerva");
-  allorders.push(cardTransferResponse); */
-
-  // Get ach
-  while (isNextExistach) {
-    const achResponse = await getRobinhoodO(withdrawalURL, "api");
-    if (isNextExistach) {
-      const achResults = achResponse.results;
-      const achMapped = achResults?.map((obj) => ({ ...obj, rhType: "ach" }));
-      isIterable(achMapped) && allorders.push(...achMapped);
-      if (
-        !achResponse.next ||
-        achResponse.next === null ||
-        achResponse.next === ""
-      ) {
-        isNextExistach = false;
-      } else {
-        withdrawalURL = achResponse.next;
-      }
-    } else {
-      isNextExistach = false;
-    }
-  }
-
-  // Get ach received
-  while (isNextExistachreceived) {
-    const receivedResponse = await getRobinhoodO(receivedURL, "api");
-    if (isNextExistachreceived) {
-      const receivedResults = receivedResponse.results;
-      const receivedMapped = receivedResults?.map((obj) => ({
-        ...obj,
-        rhType: "ach received",
-      }));
-      isIterable(receivedMapped) && allorders.push(...receivedMapped);
-      if (
-        !receivedResponse.next ||
-        receivedResponse.next === null ||
-        receivedResponse.next === ""
-      ) {
-        isNextExistachreceived = false;
-      } else {
-        receivedURL = receivedResponse.next;
-      }
-    } else {
-      isNextExistachreceived = false;
-    }
-  }
-  // Get wire transfers
-  /*   const wireResponse = await getRobinhoodO(wireURL, "api");
-  allorders.push(wireResponse); */
-  let i = 0;
-  let instruments = [];
-
-  while (isNextExistInstruments) {
-    const instrumentResponse = await getRobinhoodO(transfersURL, "bonfire");
-    if (isNextExistInstruments) {
-      const receivedResults = instrumentResponse.results;
-      const receivedMapped = receivedResults?.map((obj) => ({
-        ...obj,
-        rhType: "transfers",
-      }));
-      isIterable(receivedMapped) && allorders.push(...receivedMapped);
-      if (
-        !instrumentResponse.next ||
-        instrumentResponse.next === null ||
-        instrumentResponse.next === ""
-      ) {
-        isNextExistInstruments = false;
-      } else {
-        transfersURL = instrumentResponse.next;
-      }
-    } else {
-      isNextExistInstruments = false;
-    }
-  }
-
-  return { allorders: allorders, instruments: instruments };
+  return { allorders: allorders };
 };

101fc7451a367b3845f00da4134e3b6de34319ca
TradersIQ <62960583+TradersIQ@users.noreply.github.com>
2022-10-24 19:35:46 -0700

    only url

--- brokerHandling/robinhood/getOptionPosition.js
+++ brokerHandling/robinhood/getOptionPosition.js
@@ -3,27 +3,24 @@ const fetch = (...args) =>
 
 module.exports = async function getOptionPositionRobinhood(bodyData, req) {
   let returnObj = {};
-  const positionid = bodyData.data["id"];
+  const url = bodyData.data["url"];
   const _authToken = bodyData.data["token"];
-  const account = bodyData.data["account"];
+
   const bearerString = `Bearer ` + _authToken;
-  console.log(bodyData)
+  console.log(bodyData);
   const getOptionPositionRobinhood = async () => {
-    const response = await fetch(
-      `https://api.robinhood.com/options/positions/${account}/${positionid}`,
-      {
-        method: "GET", // *GET, POST, PUT, DELETE, etc.
-        headers: {
-          Host: "api.robinhood.com",
-          Accept: "application/json",
-          "Content-Type": "application/json",
-          "Accept-Encoding": "gzip, deflate",
-          Referer: "https://robinhood.com/",
-          Origin: "https://robinhood.com",
-          Authorization: bearerString,
-        },
-      }
-    ).catch((err) => {
+    const response = await fetch(url, {
+      method: "GET", // *GET, POST, PUT, DELETE, etc.
+      headers: {
+        Host: "api.robinhood.com",
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        "Accept-Encoding": "gzip, deflate",
+        Referer: "https://robinhood.com/",
+        Origin: "https://robinhood.com",
+        Authorization: bearerString,
+      },
+    }).catch((err) => {
       return undefined;
     });
     return response.json(); // parses JSON response into native JavaScript objects

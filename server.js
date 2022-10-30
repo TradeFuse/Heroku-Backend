16067f58503a6e70340b40c8b26bd461164a15c8
TradersIQ <62960583+TradersIQ@users.noreply.github.com>
2022-10-29 21:11:13 -0700

    console.log webhooks

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const getRobinhoodCryptoOrders = require("./brokerHandling/robinhood/getRobinhoo
 const getRobinhoodInstruments = require("./brokerHandling/robinhood/getInstrument");
 const getOptionPositionRobinhood = require("./brokerHandling/robinhood/getOptionPosition");
 const getAssetData = require("./utils/getAssetData");
-const createSession = require("./utils/stripe/createStripeSession")
+const createSession = require("./utils/stripe/createStripeSession");
 // Constants
 const PORT = process.env.PORT || 3000;
 //const HOST = "0.0.0.0";
@@ -45,6 +45,9 @@ app.listen(PORT);
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
+
+// Set your secret key. Remember to switch to your live secret key in production.
+// See your keys here: https://dashboard.stripe.com/apikeys
 
 app.post("/", async (req, res) => {
   const bodyData = req.body;
@@ -144,6 +147,59 @@ app.post("/", async (req, res) => {
       // code block
     }
   }
+});
+
+app.post("/webhook", async (req, res) => {
+  let data;
+  let eventType;
+  // Check if webhook signing is configured.
+  const webhookSecret = process.env.STRIPE_WEBHOOK_SECRET;
+  if (webhookSecret) {
+    // Retrieve the event by verifying the signature using the raw body and secret.
+    let event;
+    let signature = req.headers["stripe-signature"];
+
+    try {
+      event = stripe.webhooks.constructEvent(
+        req.body,
+        signature,
+        webhookSecret
+      );
+    } catch (err) {
+      console.log(`⚠️  Webhook signature verification failed.`);
+      return res.sendStatus(400);
+    }
+    // Extract the object from the event.
+    data = event.data;
+    eventType = event.type;
+  } else {
+    // Webhook signing is recommended, but if the secret is not configured in `config.js`,
+    // retrieve the event data directly from the request body.
+    data = req.body.data;
+    eventType = req.body.type;
+  }
+
+  switch (eventType) {
+    case "checkout.session.completed":
+      console.log("checkoiut completed")
+      // Payment is successful and the subscription is created.
+      // You should provision the subscription and save the customer ID to your database.
+      break;
+    case "invoice.paid":
+      // Continue to provision the subscription as payments continue to be made.
+      // Store the status in your database and check when a user accesses your service.
+      // This approach helps you avoid hitting rate limits.
+      break;
+    case "invoice.payment_failed":
+      // The payment failed or the customer does not have a valid payment method.
+      // The subscription becomes past_due. Notify your customer and send them to the
+      // customer portal to update their payment information.
+      break;
+    default:
+    // Unhandled event type
+  }
+
+  res.sendStatus(200);
 });
 
 discordBot();

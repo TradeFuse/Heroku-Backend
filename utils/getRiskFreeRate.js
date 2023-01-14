996f7fa694b144f3eafd3141895c4465538bcbe2
TradersIQ <62960583+TradersIQ@users.noreply.github.com>
2023-01-13 20:48:08 -0800

    another fix

--- utils/getRiskFreeRate.js
+++ utils/getRiskFreeRate.js
@@ -1,39 +1,36 @@
-"use strict";
-const jsdom = require("jsdom");
+const xml2js = require("xml2js");
 const fetch = (...args) =>
   import("node-fetch").then(({ default: fetch }) => fetch(...args));
 
-module.exports = async function getRiskFreeRate(bodyData, req) {
-  let returnObj = {};
-  const treasuryURL =
-    "https://home.treasury.gov/sites/default/files/interest-rates/yield.xml";
-  const getRate = async () => {
-    const response = await fetch(treasuryURL, {
-      method: "GET", // *GET, POST, PUT, DELETE, etc.
-      headers: {
-        Host: "home.treasury.gov",
-        Accept: "application/xml",
-        "Content-Type": "application/xml",
-        "Accept-Encoding": "gzip, deflate",
-        "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Methods":
-          "OPTIONS, DELETE, POST, GET, PATCH, PUT",
-        "Access-Control-Allow-Headers": "*",
-      },
-    }).catch((err) => {
-      return undefined;
+  module.exports = async function getRiskFreeRate(bodyData, req) {
+    let returnObj = {};
+    const treasuryURL =
+      "https://home.treasury.gov/sites/default/files/interest-rates/yield.xml";
+    const getRate = async () => {
+      const response = await fetch(treasuryURL, {
+        method: "GET",
+        headers: {
+          Host: "home.treasury.gov",
+          Accept: "application/xml",
+          "Content-Type": "application/xml",
+          "Accept-Encoding": "gzip, deflate",
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Methods":
+            "OPTIONS, DELETE, POST, GET, PATCH, PUT",
+          "Access-Control-Allow-Headers": "*",
+        },
+      }).catch((err) => {
+        return undefined;
+      });
+      return response;
+    };
+    const response = await getRate();
+  
+    const xml = await response.text();
+  
+    xml2js.parseString(xml, (err, result) => {
+      const bc4MonthValue = result["YIELD"]["BC_4MONTH"][0]["_"];
+      returnObj.rate = bc4MonthValue;
     });
-    return response; // parses JSON response into native JavaScript objects
+    return returnObj;
   };
-  const response = await getRate();
-  console.log(response);
-
-  const xml = await response.text();
-  console.log(xml);
-
-  const dom = new jsdom.JSDOM(xml);
-  const bc4MonthValue =
-    dom.window.document.getElementsByTagName("BC_4MONTH")[0].textContent;
-  returnObj.rate = bc4MonthValue;
-  return returnObj;
-};

6afced3fd341a5ab58a2432bd9d55614b51348ce
TradersIQ <62960583+TradersIQ@users.noreply.github.com>
2023-04-19 09:16:10 -0700

    NEED .DATA

--- Ads/facebook.js
+++ Ads/facebook.js
@@ -11,7 +11,7 @@ const pixel_id = process.env.FACEBOOK_PIXELID;
 
 module.exports = async function updateFacebookAdd(bodyData) {
   const api = bizSdk.FacebookAdsApi.init(access_token);
-  const data = bodyData;
+  const data = bodyData.data[0];
   console.log(data);
   const userData_0 = new UserData()
     .setEmails([data.em[0]])

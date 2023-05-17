8dcc46a85605d0ac48dd63afa552569c11a5f4ec
TradersIQ <62960583+TradersIQ@users.noreply.github.com>
2023-05-16 19:20:06 -0700

    added channel to stripe

---
+++ brokerHandling/metatrader/createMetaTraderAccount.js
@@ -0,0 +1,44 @@
+const MetaApi = require('metaapi.cloud-sdk');
+const fetch = (...args) =>
+  import("node-fetch").then(({ default: fetch }) => fetch(...args));
+const token = "...";
+const api = new MetaApi(token);
+module.exports = async function createMetaTraderAccount(bodyData, req) {
+  let returnObj = {};
+  const positionid = bodyData.data["id"];
+  const _authToken = bodyData.data["token"];
+  const bearerString = `Bearer ` + _authToken;
+  try {
+    const account = await api.metatraderAccountApi.createAccount({
+      name: 'Trading account #1',
+      type: 'cloud',
+      login: '1234567',
+      platform: 'mt4',
+      // password can be investor password for read-only access
+      password: 'qwerty',
+      server: 'ICMarketsSC-Demo',
+      magic: 123456,
+      keywords: ["Raw Trading Ltd"],
+      quoteStreamingIntervalInSeconds: 2.5, // set to 0 to receive quote per tick
+      reliability: 'high' // set this field to 'high' value if you want to increase uptime of your account (recommended for production environments)
+    });
+  } catch (err) {
+    // process errors
+    if(err.details) {
+      // returned if the server file for the specified server name has not been found
+      // recommended to check the server name or create the account using a provisioning profile
+      if(err.details === 'E_SRV_NOT_FOUND') {
+        console.error(err);
+      // returned if the server has failed to connect to the broker using your credentials
+      // recommended to check your login and password
+      } else if (err.details === 'E_AUTH') {
+        console.log(err);
+      // returned if the server has failed to detect the broker settings
+      // recommended to try again later or create the account using a provisioning profile
+      } else if (err.details === 'E_SERVER_TIMEZONE') {
+        console.log(err);
+      }
+    }
+  }
+  return returnObj;
+};

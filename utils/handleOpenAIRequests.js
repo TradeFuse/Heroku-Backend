77c524c56d8818204d2d3817fc7953fc323f10cc
TradersIQ <62960583+TradersIQ@users.noreply.github.com>
2023-11-22 13:55:14 -0800

    upgrade to gpt4

--- utils/handleOpenAIRequests.js
+++ utils/handleOpenAIRequests.js
@@ -9,7 +9,7 @@ module.exports = async function handleOpenAIRequest(bodyData) {
   let message = "";
   try {
     const completion = await openai.createChatCompletion({
-      model: "gpt-3.5-turbo",
+      model: "gpt-4-1106-preview",
       messages: [{ role: "user", content: bodyData.data["message"] }],
     });
     message = completion.data.choices[0].message;

28c8a87a5717b4030009a69472c2424e2977fb30
TradersIQ <62960583+TradersIQ@users.noreply.github.com>
2024-03-16 11:50:05 -0700

    consoole.losubscriptioon

--- server.js
+++ server.js
@@ -565,8 +565,8 @@ app.post(
         const stripeId = checkoutSessionCompleted.customer;
         const retrievedCustomer = await getCustomer(stripeId);
         const metadata = checkoutSessionCompleted?.metadata;
-        const subscription = retrievedCustomer?.subscription;
-        console.log(retrievedCustomer);
+        const subscription = retrievedCustomer?.subscriptions.data[0];
+        console.log(subscription);
         const subscriptionEnd = subscription?.current_period_end;
         const Auth0User = metadata?.auth0id;
         const S3InputData = {

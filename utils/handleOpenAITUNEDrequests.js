3876d1f6e8f509d69fa31bb71615b31eb2be9639
Eric Holmes <ericholmes34@gmail.com>
2024-04-05 21:00:17 -0700

    chajneg tune

--- utils/handleOpenAITUNEDrequests.js
+++ utils/handleOpenAITUNEDrequests.js
@@ -9,7 +9,7 @@ module.exports = async function handleOpenAITUNEDrequests(bodyData) {
   let message = "";
   try {
     const completion = await openai.createChatCompletion({
-      model: "ft:gpt-3.5-turbo-0125:tradefuse:masterai2:9AqsuAh5",
+      model: "ft:gpt-3.5-turbo-0125:tradefuse:masteraimessage1:9ArVNiyr",
       messages: [{ role: "user", content: bodyData.data["message"] }],
     });
     message = completion.data.choices[0].message;
